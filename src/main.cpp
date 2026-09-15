67e98ed7e5c1e8ef00bc21f693bffaa184eab42b
RashLop <lopezmejiarasshid@outlook.com>
2026-09-15 13:01:09 -0400

    feat: logs de distancia leida agregado

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include <Arduino.h>
+
 #include "DistanceIndicator.h"
 #include "LedDriver.h"
 #include "UltrasonicSensor.h"
@@ -10,16 +11,45 @@ LedDriver leds(25, 26, 27);
 }
 
 void setup() {
+    Serial.begin(115200);
+    delay(500);
+
     leds.begin();
+
     indicator = DistanceIndicator(millis());
+
     leds.apply(indicator.output(millis()));
+
     sensor.begin();
 }
 
 void loop() {
     Reading reading;
-    if (sensor.takeReading(reading)) indicator.accept(reading.distanceCm, millis());
+
+    if (sensor.takeReading(reading)) {
+
+        if (reading.distanceCm.has_value()) {
+            Serial.print("Distance: ");
+            Serial.print(*reading.distanceCm);
+            Serial.println(" cm");
+        } else {
+            Serial.println("Distance: INVALID");
+        }
+
+        indicator.accept(reading.distanceCm, millis());
+    }
+
     sensor.update();
-    if (sensor.takeReading(reading)) indicator.accept(reading.distanceCm, millis());
+
+    if (sensor.takeReading(reading)) {
+
+        if (reading.distanceCm.has_value()) {
+            Serial.print("Distance: ");
+            Serial.print(*reading.distanceCm);
+            Serial.println(" cm");
+        }
+        indicator.accept(reading.distanceCm, millis());
+    }
+
     leds.apply(indicator.output(millis()));
 }
